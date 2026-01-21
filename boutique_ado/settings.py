1648d24be6327d0cf22ba915bc2f12e9560a744a
dylanmcca <dylanmcca@gmail.com>
2026-01-21 21:42:20 +0000

    connected to stripe & set up Webhooks & tested them

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = 'django-insecure-bf_nx7m0sd(kbofoqv1g*ba))$ux)2(ju#mr-kr5#21@5iql_d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
