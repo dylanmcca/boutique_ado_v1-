fe7990ab83d53cb58639518a2b974dc22a1ece25
dylanmcca <dylanmcca@gmail.com>
2026-01-25 12:02:21 +0000

    updated checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,8 +1,16 @@
 from django import forms
+from django_countries.fields import CountryField
 from .models import Order
 
 
 class OrderForm(forms.ModelForm):
+    # Override the country field with a regular ChoiceField
+    # to avoid crispy_forms rendering issues with BlankChoiceIterator
+    country = forms.ChoiceField(
+        required=True,
+        widget=forms.Select(attrs={'class': 'custom-select d-block', 'required': 'required'}),
+    )
+    
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
@@ -16,24 +24,32 @@ class OrderForm(forms.ModelForm):
         labels and set autofocus on first field
         """
         super().__init__(*args, **kwargs)
+        
+        # Get the country choices from the model field and materialize them
+        country_field = Order._meta.get_field('country')
+        self.fields['country'].choices = [('', 'Country *')] + list(country_field.choices)
+        
         placeholders = {
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
+        
+        # Add styling to make country placeholder appear disabled/greyed out
+        self.fields['country'].widget.attrs['class'] = 'stripe-style-input'
