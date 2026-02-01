3a45b6113695abb185e5c86697bbd7d4308c79b1
dylanmcca <dylanmcca@gmail.com>
2026-02-01 17:51:01 +0000

    corrected trailing space error for static file upload to aws

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -186,7 +186,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'botique-ado-ceee363a3f56 ' # change this to your AWS bucket name
+    AWS_STORAGE_BUCKET_NAME = 'botique-ado-ceee363a3f56'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
