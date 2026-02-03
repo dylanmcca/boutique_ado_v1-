93bdbb0fa7e3bd2af8ea4df04facfbc6657eecbb
dylanmcca <dylanmcca@gmail.com>
2026-02-03 09:55:03 +0000

    Style verification messages as toast notifications in top right corner

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True if 'DEVELOPMENT' in os.environ else False
 
 ALLOWED_HOSTS = [
     'localhost',
